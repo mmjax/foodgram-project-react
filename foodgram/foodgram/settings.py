ae5711c56e9cf217a7a03688c04e94fad01dc496
Максим <maxim.skalozubov@yandex.ru>
2022-07-18 23:38:35 +0300

    UPDATE: settings(media dir)

--- foodgram/foodgram/settings.py
+++ foodgram/foodgram/settings.py
@@ -93,8 +93,11 @@ AUTH_USER_MODEL = 'users.User'
 
 STATIC_URL = '/static/'
 
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static/'),)
 
+MEDIA_URL = '/media/'
 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
